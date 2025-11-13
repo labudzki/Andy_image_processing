b54d8b5433e92d18ae76811a21e1422d242a97f7
andrea <andrealabudzki@gmail.com>
2025-11-13 15:09:11 +0100

    3d demo working

--- Scripts/Demo_3D_visu.py
+++ Scripts/Demo_3D_visu.py
@@ -2,10 +2,12 @@ import napari
 import numpy as np
 from tifffile import TiffFile
 from pathlib import Path
-import utils.tifreading as u_tif
+#import utils.tifreading as u_tif
+
+fnum = 3
 
 path_movie = Path(
-    r"D:\AMOLF-SHIMIZU Dropbox\DATA\Ach_data\5. Lipids and Organelles imaging\RawData\250711\TES2505A017\Run15\Run15_MMStack_Pos0.ome.tif"
+rf"c:\Users\labudzki\OneDrive - AMOLF\Desktop\Data\HSE2508A264\Run{fnum}\Run{fnum}_MMStack_Pos0.ome.tif"
 )
 
 # Load as a NumPy array (ensure it’s contiguous)
@@ -13,7 +15,7 @@ with TiffFile(path_movie) as tif:
     stack = tif.asarray()
     
 
-stack=u_tif.load_tiff_stack(path_movie, squeeze=True)
+#stack=u_tif.load_tiff_stack(path_movie, squeeze=True)
 
 stack = np.array(stack, dtype='float32')  # reduce memory usage
 
@@ -21,5 +23,15 @@ print(stack.shape, stack.dtype)  # sanity check
 
 
 viewer = napari.Viewer()
-viewer.add_image(stack, name='Raw video', multiscale=False, axis_labels=['Time','Y','X'])
+
+viewer.add_image(stack, name='Raw video', multiscale=False, axis_labels=['Time','Z','Y','X'])
+# viewer.add_image(stack, name='Raw video', multiscale=False, axis_labels=['Z','Y','X']) # for Run1 and Run2
+# viewer.add_image(stack, name='Raw video', multiscale=False, axis_labels=['Time','Y','X']) # for Run5
+# #print(viewer.layers.data
 napari.run()
+
+
+# # ✅ Cleanup after viewer closes
+# viewer.close()       # Close Napari viewer
+# del stack            # Delete stack variable
+# gc.collect()         # Force garbage collection
