f390ae66ad4dc3814056ba3461d0bb9562f4dd97
labudzki <57876872+labudzki@users.noreply.github.com>
2026-03-27 09:50:37 +0100

    updated the python version to be compatible. changed \ to / in file pathnames

--- Python Scripts/3D_viewer_napari.py
+++ Python Scripts/3D_viewer_napari.py
@@ -11,7 +11,8 @@ path_movie = Path(
 # rf"C:\Users\labudzki\OneDrive - AMOLF\Documents\Repositories\confocal_processing\lipid movies\SAL2506A042\Mov21_MMStack_Pos0.ome.tif" #magnification 50X
 # rf"C:\Users\labudzki\AMOLF-SHIMIZU Dropbox\DATA\Ach_data\5. Lipids and Organelles imaging\RawData\251125\CFL2510A002\Run7\Run7_MMStack_Pos0.ome.tif"
 # rf"C:\Users\labudzki\AMOLF-SHIMIZU Dropbox\DATA\Ach_data\5. Lipids and Organelles imaging\RawData\251125\CFL2510A002\Run10\Run10_MMStack_Pos0.ome.tif"
-rf"C:\Users\labudzki\AMOLF-SHIMIZU Dropbox\DATA\Ach_data\5. Lipids and Organelles imaging\RawData\251128\CFL2510A005\Run5\Run5_MMStack_Pos0.ome.tif"
+# rf"C:\Users\labudzki\AMOLF-SHIMIZU Dropbox\DATA\Ach_data\5. Lipids and Organelles imaging\RawData\251128\CFL2510A005\Run5\Run5_MMStack_Pos0.ome.tif"
+rf"/Users/andrealabudzki/Library/CloudStorage/Dropbox-AMOLF-SHIMIZU/DATA/Ach_data/5. Lipids and Organelles imaging/RawData/251128/CFL2510A005/Run5/Run5_MMStack_Pos0.ome.tif"
 )
 
 # Load as a NumPy array (ensure it’s contiguous)
